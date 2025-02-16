925a7a3b24f4257c1770b1e619eee4eeeab6917a
Yaten Dhingra <yaten598@gmail.com>
2025-02-16 21:50:38 +0530

    added classnames pkg and updated color plaette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,27 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: "#605CFF",
+        secondary: "#FF69B4",
+        accent: "#2FE5A7",
+        neutral: "#FFFFFF",
+        background: "#1A202C",
+        grey: "#364153",
+        border: "#2C3240",
+      },
+
+      fontSize: {
+        DisplayLarge: "32px",
+        DisplayMedium: "30px",
+        DisplaySmall: "28px",
+        HeadingLarge: "26px",
+        HeadingMedium: "24px",
+        HeadingSmall: "22px",
+        TitleLarge: "20px",
+        TitleMedium: "18px",
+        TitleSmall: "16px",
+      },
     },
   },
   plugins: [],
