23fab9866ae6f0cbe6daf85d2c8778ccdb3e5122
Yaten Dhingra <yaten598@gmail.com>
2025-03-16 23:16:25 +0530

    remove forwardRef from Button component

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -1,28 +1,30 @@
+"use client";
+
 import classNames from "classnames";
-import { forwardRef } from "react";
-import * as React from "react";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = forwardRef<
-  HTMLButtonElement,
-  ButtonProps
->(({ children, className, disabled = false, ...restProps }, ref) => (
-  <button
-    ref={ref}
-    className={classNames(
-      disabled
-        ? "bg-[#A09DFF] cursor-not-allowed text-white min-w-[138px] h-[59px] px-[40px] py-[18px] text-center rounded-[10px] w-full"
-        : "cursor-pointer rounded-[10px] text-white bg-primary min-w-[138px] h-[59px] px-[40px] py-[18px] text-center w-full",
-      className,
-    )}
-    disabled={disabled}
-    {...restProps}
-  >
-    {children}
-  </button>
-));
-
-Button.displayName = "Button";
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  disabled = false,
+  ...restProps
+}) => {
+  return (
+    <button
+      className={classNames(
+        "rounded-[10px] text-white min-w-[138px] h-[59px] px-[40px] py-[18px] text-center w-full",
+        disabled
+          ? "bg-[#A09DFF] cursor-not-allowed"
+          : "cursor-pointer bg-primary",
+        className,
+      )}
+      disabled={disabled}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
