ad41d1ccdc59902bc876ae263a2ad4fb83d72684
Yaten Dhingra <yaten598@gmail.com>
2025-03-21 13:38:33 +0530

    implimented auth with redirects

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
-"use client";
+import { signOut } from "@/lib/auth/auth";
 
-import * as React from "react";
-import { useRouter, usePathname } from "next/navigation";
-import { retrieveUser } from "@/lib/auth/auth";
+import Button from "@/components/form/Button";
 
 export default function Home() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  React.useEffect(() => {
-    async function checkUser() {
-      const session = await retrieveUser();
-
-      if (session && pathname !== "/") {
-        // If user is signed in and NOT on '/', redirect to home
-        router.replace("/");
-      } else if (!session && pathname !== "/signin") {
-        // If user is NOT signed in and NOT on '/signin', redirect to signin
-        router.replace("/signin");
-      }
-    }
-
-    checkUser();
-  }, []);
-
   return (
     <main className="flex items-center justify-center h-screen">
-      Hello World <br />
-      !!This is a temporary logic... Needs to be checked if working fine or
-      not!!
+      <h1>Welcome to Home Page</h1>
+      <Button onClick={signOut}>Sign out</Button>
     </main>
   );
 }
