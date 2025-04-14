b4c7ab3d41417a6f9fea06867f1c02f5ae529a62
Yaten Dhingra <yaten598@gmail.com>
2025-04-14 13:39:45 +0530

    add SessionProvider and TeacherProvider to root file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import { SessionProvider } from "@/contexts/sessionContext";
+import { TeacherProvider } from "@/contexts/teacherContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionProvider>
+          <TeacherProvider>{children}</TeacherProvider>
+        </SessionProvider>
       </body>
     </html>
   );
