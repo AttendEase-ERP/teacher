46a4e83ec1bc6e68d508c836953c45526641069e
Yaten Dhingra <yaten598@gmail.com>
2025-04-14 13:41:29 +0530

    run prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import { signOut } from "@/lib/auth/auth";
 
 import Button from "@/components/form/Button";
 
+import { useTeacherContext } from "@/contexts/teacherContext";
+
 export default function Home() {
+  const teacherDetails = useTeacherContext();
+
   return (
     <main className="flex items-center justify-center h-screen">
       <h1>Welcome to Home Page</h1>
       <Button onClick={signOut}>Sign out</Button>
+
+      <p>
+        Teacher name:{" "}
+        {teacherDetails ? teacherDetails.name : "No teacher found"}
+      </p>
     </main>
   );
 }
