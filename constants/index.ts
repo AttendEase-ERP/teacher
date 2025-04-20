c807db382d257104e79009c3c1a5f234094c0857
Yaten Dhingra <yaten598@gmail.com>
2025-04-20 20:04:12 +0530

    add image route for home page icons

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+const SideBarLinks = {
+  Dashboard: "/home/dashboard.svg",
+  Sharing: "/home/sharing.svg",
+  Quiz: "/home/quiz.svg",
+  Chat: "/home/chat.svg",
+  Calendar: "/home/calendar.svg",
+  Notifications: "/home/notifications.svg",
+  Settings: "/home/settings.svg",
+};
+
+export { SideBarLinks };
