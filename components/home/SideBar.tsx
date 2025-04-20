95650ed7fa2e29689b0f4e985ccf7cb3b1be9b31
Yaten Dhingra <yaten598@gmail.com>
2025-04-20 22:41:29 +0530

    add signOut button in sideBar

--- components/home/SideBar.tsx
+++ components/home/SideBar.tsx
@@ -3,33 +3,44 @@
 import { SideBarLinks } from "@/constants";
 
 import SideBarLink from "./SideBarLink";
-
 import NextImage from "../Image";
+import Button from "../form/Button";
+
+import { signOut } from "@/lib/auth/auth";
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col items-start justify-start w-[25%] h-screen p-4 bg-neutral border-r">
-      <div className="items-center justify-center w-full h-[10%] flex flex-col mt-6 text-primary font-bold text-[22px]">
-        <NextImage
-          src="/logo.svg"
-          alt=""
-          width={60}
-          height={60}
-          className="mb-4"
-        />
-        <h1>AttendEase</h1>
+    <div className="flex flex-col justify-between w-[25%] h-screen p-4 bg-neutral border-r">
+      <div>
+        <div className="items-center justify-center w-full h-[10%] flex flex-col mt-6 text-primary font-bold text-[22px]">
+          <NextImage
+            src="/logo.svg"
+            alt="AttendEase logo"
+            width={60}
+            height={60}
+            className="mb-4"
+          />
+          <h1>AttendEase</h1>
+        </div>
+
+        <nav className="flex flex-col gap-2 mt-10">
+          {Object.entries(SideBarLinks).map(([label, iconPath]) => (
+            <SideBarLink
+              key={label}
+              href={label !== "Dashboard" ? `/${label.toLowerCase()}` : "/"}
+              label={label}
+              icon={iconPath}
+            />
+          ))}
+        </nav>
       </div>
 
-      <nav className="flex flex-col gap-2 mt-10">
-        {Object.entries(SideBarLinks).map(([label, iconPath]) => (
-          <SideBarLink
-            key={label}
-            href={label !== "Dashboard" ? `/${label.toLowerCase()}` : "/"}
-            label={label}
-            icon={iconPath}
-          />
-        ))}
-      </nav>
+      <Button
+        onClick={signOut}
+        className="text-left mt-6 text-sm font-medium text-red-600 bg-red-100 rounded transition-colors duration-200"
+      >
+        Sign out
+      </Button>
     </div>
   );
 };
