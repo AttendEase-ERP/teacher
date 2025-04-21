88e2326725695333d31a6f85fb2b9877137a2a6a
Yaten Dhingra <yaten598@gmail.com>
2025-04-21 16:12:57 +0530

    update Button component to add overrideStyles prop

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -2,26 +2,34 @@
 
 import classNames from "classnames";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+import React from "react";
+
+interface CustomButtonProps {
+  overrideStyles?: boolean;
+}
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  CustomButtonProps;
 
 const Button: React.FC<ButtonProps> = ({
   children,
   className,
   disabled = false,
+  overrideStyles = false,
   ...restProps
 }) => {
-  return (
-    <button
-      className={classNames(
+  const computedClassName = overrideStyles
+    ? classNames(className)
+    : classNames(
         "rounded-[10px] text-white min-w-[138px] h-[59px] px-[40px] py-[18px] text-center w-full",
         disabled
           ? "bg-[#A09DFF] cursor-not-allowed"
           : "cursor-pointer bg-primary",
         className,
-      )}
-      disabled={disabled}
-      {...restProps}
-    >
+      );
+
+  return (
+    <button className={computedClassName} disabled={disabled} {...restProps}>
       {children}
     </button>
   );
