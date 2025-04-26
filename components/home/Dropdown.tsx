ab3856ff06d162f8a9244282d825b9de340b2f01
Yaten Dhingra <yaten598@gmail.com>
2025-04-26 20:23:38 +0530

    update dropdown from hover to onlcick

--- components/home/Dropdown.tsx
+++ components/home/Dropdown.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { useState } from "react";
-
 import { ChevronDown } from "lucide-react";
-
 import Button from "../form/Button";
-
 import classNames from "classnames";
 
 interface SubjectDropdownProps {
@@ -18,38 +15,43 @@ const TeacherSubjectDropdown = ({
   onSelect,
 }: SubjectDropdownProps) => {
   const [selectedSubject, setSelectedSubject] = useState("Subject");
-  const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleSelect = (subject: string) => {
     setSelectedSubject(subject);
     onSelect(subject);
-    setIsHovered(false);
+    setIsOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
   };
 
   return (
-    <div
-      className="relative inline-block text-left w-[120px]"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="relative inline-block text-left w-[120px]">
       <Button
         className="flex items-center justify-between bg-primary px-4 hover:bg-[#5b52e0] w-[120%] h-10 text-sm rounded-[10px] duration-200 text-white text-center"
         overrideStyles={true}
+        onClick={toggleDropdown}
       >
         {selectedSubject}
-        <ChevronDown size={16} className="text-white" />
+        <ChevronDown
+          size={16}
+          className={classNames(
+            "text-white transition-transform duration-300",
+            isOpen ? "rotate-180" : "rotate-0",
+          )}
+        />
       </Button>
 
-      {isHovered && (
+      {isOpen && (
         <div className="absolute z-10 mt-1 w-[120%] bg-white rounded-md shadow-md overflow-hidden ring-1 ring-black ring-opacity-5">
           <ul className="text-sm">
             {subjects.map((subject, index) => (
               <li
                 key={index}
                 onClick={() => handleSelect(subject)}
-                className={classNames(
-                  "px-4 py-2 cursor-pointer hover:bg-gray-100 text-gray-700",
-                )}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100 text-gray-700"
               >
                 {subject}
               </li>
