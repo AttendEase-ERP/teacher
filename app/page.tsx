78547380389d5c49e31ffca3012ea23c672a75aa
Yaten Dhingra <yaten598@gmail.com>
2025-05-18 17:47:48 +0530

    temp: migrate from context to idb

--- app/page.tsx
+++ app/page.tsx
@@ -2,67 +2,201 @@
 
 import * as React from "react";
 
-import Dropdown from "@/components/home/Dropdown";
 import SideBar from "@/components/home/SideBar";
+import Dropdown from "@/components/home/Dropdown";
 import ClassCardList from "@/components/home/ClassCard";
 
-import { useTeacherContext } from "@/contexts/teacherContext";
+import { idb, Teachers } from "@/lib/indexedDB/idb";
+
+import { getTeacherEmail } from "@/lib/auth/auth";
+
+import { createClient } from "@/utils/supabase/client";
+
+import { formatTeacherData } from "@/utils/utils";
+
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const teacherDetails = useTeacherContext();
+  const [subjectsList, setSubjectsList] = React.useState<string[]>([]);
 
-  const [selectedSubject, setSelectedSubject] = React.useState<string | null>(
-    null,
-  );
-  const [selectedClassDetails, setSelectedClassDetails] = React.useState<
-    | {
-        semester: number;
-        section: string;
-      }[]
-    | null
-  >(null);
-
-  const teacherSubjects =
-    teacherDetails?.Teacher_Section_Assignment.map(
-      (assignment) => assignment.Subjects.subject_name,
-    ) || [];
-
-  const handleSubjectSelect = (subject: string) => {
-    setSelectedSubject(subject);
+  const handleSubjectSelect = (selectedSubject: string) => {
+    console.log(selectedSubject);
   };
 
   React.useEffect(() => {
-    if (selectedSubject && teacherDetails) {
-      const filteredClasses = teacherDetails.Teacher_Section_Assignment.filter(
-        (assignment) => assignment.Subjects.subject_name === selectedSubject,
-      ).map((assignment) => ({
-        semester: assignment.Subjects.semester,
-        section: assignment.Sections.section_name,
-      }));
-      setSelectedClassDetails(filteredClasses);
-    } else {
-      setSelectedClassDetails([]);
-    }
-  }, [selectedSubject, teacherDetails]);
+    const fetchTeacherDetails = async () => {
+      try {
+        const supabase = createClient();
+        const teacherEmail = await getTeacherEmail();
+        const teacherDataFromIDB = await idb.Teachers.where("email")
+          .equals(teacherEmail!)
+          .toArray();
+        const fetchTeacherDataFromDB = async () => {
+          const { data, error } = await supabase
+            .from("Teacher_Section_Assignment")
+            .select(
+              `
+              *,
+              Teachers(*),
+              Sections(
+              *,
+              Courses(*)
+              ),
+              Subjects(*)
+              `,
+            )
+            .eq("Teachers.email", teacherEmail!);
+          return { data, error };
+        };
 
+        supabase
+          .channel("teacher_data")
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "Teachers",
+              filter: `email=eq.${teacherEmail}`,
+            },
+            async (payload) => {
+              console.log(payload);
+              const { data, error } = await fetchTeacherDataFromDB();
+              if (!error) {
+                const newFormattedTeacherData: Teachers[] = formatTeacherData(
+                  data!,
+                );
+                console.log(newFormattedTeacherData);
+              }
+            },
+          )
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "Teacher_Section_Assignment",
+              filter: `Teachers.email=eq.${teacherEmail}`,
+            },
+            async (payload) => {
+              console.log(payload);
+              const { data, error } = await fetchTeacherDataFromDB();
+              if (!error) {
+                const newFormattedTeacherData: Teachers[] = formatTeacherData(
+                  data!,
+                );
+                console.log(newFormattedTeacherData);
+              }
+            },
+          )
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "Subjects",
+              filter: `Teachers.email=eq.${teacherEmail}`,
+            },
+            async (payload) => {
+              console.log(payload);
+              const { data, error } = await fetchTeacherDataFromDB();
+              if (!error) {
+                const newFormattedTeacherData: Teachers[] = formatTeacherData(
+                  data!,
+                );
+                console.log(newFormattedTeacherData);
+              }
+            },
+          )
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "Sections",
+              filter: `Teachers.email=eq.${teacherEmail}`,
+            },
+            async (payload) => {
+              console.log(payload);
+              const { data, error } = await fetchTeacherDataFromDB();
+              if (!error) {
+                const newFormattedTeacherData: Teachers[] = formatTeacherData(
+                  data!,
+                );
+                console.log(newFormattedTeacherData);
+              }
+            },
+          )
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "Courses",
+              filter: `Teachers.email=eq.${teacherEmail}`,
+            },
+            async (payload) => {
+              console.log(payload);
+              const { data, error } = await fetchTeacherDataFromDB();
+              if (!error) {
+                const newFormattedTeacherData: Teachers[] = formatTeacherData(
+                  data!,
+                );
+                console.log(newFormattedTeacherData);
+              }
+            },
+          )
+          .subscribe();
+
+        if (teacherDataFromIDB.length > 0) {
+          // now add realtime here, if any result changes, then it'll be automatically updated in IDB.
+          const uniqueSubjects = Array.from(
+            new Set(teacherDataFromIDB.map((item) => item.subject_name)),
+          );
+
+          setSubjectsList(uniqueSubjects);
+        } else {
+          const { data, error } = await fetchTeacherDataFromDB();
+
+          if (!error && data) {
+            const formattedTeacherDataForIDBStore = formatTeacherData(data);
+            await idb.Teachers.bulkPut(formattedTeacherDataForIDBStore);
+            const uniqueSubjects = Array.from(
+              new Set(
+                formattedTeacherDataForIDBStore.map(
+                  (item) => item.subject_name,
+                ),
+              ),
+            );
+
+            setSubjectsList(uniqueSubjects);
+          } else {
+            console.error(error);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTeacherDetails();
+  }, []);
   return (
     <main className="h-screen bg-backgroundLight flex flex-row gap-10">
       <SideBar />
 
       <div className="flex flex-col w-full h-screen bg-backgroundLight gap-10">
         <h1 className="font-bold text-[25px] mt-4">Dashboard</h1>
+        <Dropdown subjects={subjectsList} onSelect={handleSubjectSelect} />
 
-        <Dropdown subjects={teacherSubjects} onSelect={handleSubjectSelect} />
-
-        <ClassCardList
+        {/* <ClassCardList
           classes={selectedClassDetails}
           onClick={() =>
             redirect(
               `/class/${selectedClassDetails![0].semester}/${selectedClassDetails![0].section.toLowerCase()}`,
             )
           }
-        />
+        /> */}
       </div>
     </main>
   );
---
+++ utils/utils.ts
@@ -0,0 +1,24 @@
+import { Teachers } from "@/lib/indexedDB/idb";
+
+import { teacherType } from "@/types/teacher/teacher";
+
+const formatTeacherData = (teacherData: teacherType): Teachers[] => {
+  if (!teacherData) return [];
+
+  return teacherData.map((item) => ({
+    id: item.id,
+    name: item.Teachers.name,
+    email: item.Teachers.email,
+    section_id: item.section_id,
+    subject_id: item.subject_id,
+    teacher_id: item.teacher_id,
+    subject_name: item.Subjects.subject_name,
+    semester: item.Subjects.semester,
+    section_name: item.Sections.section_name,
+    course_id: item.Sections.course_id,
+    course_name: item.Sections.Courses.course_name,
+    course_duration: item.Sections.Courses.course_duration,
+  }));
+};
+
+export { formatTeacherData };
